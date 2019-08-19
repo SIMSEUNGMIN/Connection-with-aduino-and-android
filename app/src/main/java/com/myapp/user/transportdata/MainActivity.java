0088365a9cb34b625ed486ee52a21920c0dc8d21
SIMSEUNGMIN <ssm4591@naver.com>
2019-08-20 02:06:22 +0900

    add function that connect with aduino

--- app/src/main/java/com/myapp/user/transportdata/MainActivity.java
+++ app/src/main/java/com/myapp/user/transportdata/MainActivity.java
@@ -2,6 +2,7 @@ package com.myapp.user.transportdata;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -18,9 +19,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import static app.akexorcist.bluetotohspp.library.BluetoothState.REQUEST_ENABLE_BT;
 
@@ -43,8 +46,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //Using the bluetooth
     private BluetoothAdapter btAdapter = null;
 
-    //paired devices
-    private ArrayAdapter<String> arrayAdapter = null;
+    //페어링된 기기 목록
+    Set<BluetoothDevice> pairedDevices = null;
+
+    //페어링 된 블루투스 장치의 이름, 맥주소 목록
+    List<String> pairedDevicesList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,11 +114,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toast.makeText(this, "블루투스가 활성화 되어있습니다.", Toast.LENGTH_SHORT).show();
     }
 
-    private void getPairedDevices() {
+    private void getPairedDevices(final int numberOfId) {
         //연결하고자 하는 장치와 이미 페어링이 이루어져있다고 가정
         //Toast.makeText(this, "연결 가능한 기기 목록을 불러옵니다.", Toast.LENGTH_SHORT).show();
 
-        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        pairedDevices = btAdapter.getBondedDevices();
 
         if(pairedDevices.size() > 0){
             //페어링된 장치가 있는 경우
@@ -122,20 +128,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             AlertDialog.Builder btBuilder = new AlertDialog.Builder(this);
             btBuilder.setTitle("블루투스 장치 선택");
 
-            //페어링 된 블루투스 장치의 이름 목록 작성
-            List<String> pairedDevicesList = new ArrayList<String>();
+            //페어링 된 블루투스 장치의 이름, 맥주소 목록 작성
+            pairedDevicesList = new ArrayList<String>();
 
             for(BluetoothDevice device : pairedDevices){
                 pairedDevicesList.add(device.getName() + "\n" + device.getAddress());
             }
 
-            CharSequence[] devices = pairedDevicesList.toArray(new CharSequence[pairedDevicesList.size()]);
+            final CharSequence[] devices = pairedDevicesList.toArray(new CharSequence[pairedDevicesList.size()]);
 
             btBuilder.setItems(devices, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int number) {
-                    //연결할 장치를 눌렀을 경우 선택한 장치와 연결을 시도
-                    
+                    //연결하고자 하는 장치의  bluetoothDevice 객체 불러오기
+                    BluetoothDevice selectDevice = getDevice(pairedDevicesList.get(number));
+
+                    //RECV일 경우 아두이노와 연결
+                    //SEND일 경우 안드로이드와 연결
+                    if(numberOfId == R.id.RECVConnect){
+                        connectWithAduino(selectDevice);
+                    }
+                    else{
+                        connectWithAdroid(selectDevice);
+                    }
+
                 }
             });
 
@@ -149,16 +165,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private BluetoothDevice getDevice(String data){
+        BluetoothDevice selectDevice = null;
+
+        String[] splitData = data.split("\n");
+
+        String name = splitData[0];
+        String address = splitData[1];
+
+        for(BluetoothDevice device : pairedDevices){
+            if(name.equals(device.getName()) && address.equals(device.getAddress())){
+                selectDevice = device;
+                break;
+            }
+        }
+
+        return selectDevice;
+    }
+
+    private void connectWithAduino(BluetoothDevice selectDevice){
+        UUID uuid = UUID.fromString("000011001-0000-1000-8000-00805F9B34FB");
+
+        try{
+            //소켓 생성
+            BluetoothSocket aduinoSocket = selectDevice.createRfcommSocketToServiceRecord(uuid);
+            //RMCOMM 채넣을 통한 연결
+            aduinoSocket.connect();
+
+        } catch (IOException e) {
+            Toast.makeText(this, "연결 실패", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    private void connectWithAdroid(BluetoothDevice selectDevice){
+
+    }
+
     @Override
     public void onClick(View v){
         switch (v.getId()){
             case R.id.RECVConnect:
                 //페어링된 원격 디바이스 목록 구하기
-                getPairedDevices();
+                getPairedDevices(v. getId());
                 break;
             case R.id.SENDConnect:
                 //페어링된 원격 디바이스 목록 구하기
-                getPairedDevices();
+                getPairedDevices(v. getId());
                 break;
         }
     }
