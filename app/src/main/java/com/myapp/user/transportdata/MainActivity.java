faaed2520e810d0e848f212784ec68350c8779ec
SIMSEUNGMIN <ssm4591@naver.com>
2019-08-23 11:29:18 +0900

    clean code

--- app/src/main/java/com/myapp/user/transportdata/MainActivity.java
+++ app/src/main/java/com/myapp/user/transportdata/MainActivity.java
@@ -13,11 +13,9 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
